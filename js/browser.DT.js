e198aa07b7b4adf34b63325f95e08150333a7fdc
Scott Lewis <scooterpsu@gmail.com>
2015-08-19 19:02:31 -0400

    Ordered new data, cleaned up code
    Less hackey. Now to add functionality!

--- js/browser.DT.js
+++ js/browser.DT.js
@@ -9,21 +9,21 @@ var selectedID = 0;
 var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 $(document).ready(function() {
-    var t = $('#serverTable').DataTable( {
+    var table = $('#serverTable').DataTable( {
         columns: [
-            { title: "Name"},
+            { title: "ID", visible: false},
+            { title: "IP" },
+			{ title: "Name" },
             { title: "Host" },
 			{ title: "Map" },
-            { title: "Map File" },
+            { title: "Map File"},
             { title: "Variant" },
             { title: "Variant Type" },
             { title: "Status" },
 			{ title: "Num Players" },
             { title: "Max Players" },
-			{ title: "Version" },
-            { title: "Private" },
-			{ title: "ID"},
-			{ title: "IP" }
+			{ title: "Private" },
+            { title: "Version" }
         ]
     } );
 var jqhxr = $.getJSON( "http://192.99.124.162/list", null)
@@ -48,13 +48,6 @@ var jqhxr = $.getJSON( "http://192.99.124.162/list", null)
 									*/
                                     var jqhrxServerInfo = $.getJSON("http://" + serverIP, null )
                                     .done(function(serverInfo) {
-                                            if (serverInfo.hasOwnProperty("passworded")){
-                                                //console.log("passworded");
-                                                delete serverInfo.passworded;
-                                                serverInfo["passworded"] = true;
-                                            } else {
-                                                serverInfo["passworded"] = false;
-                                            }
                                             serverInfo["serverId"] = i;
                                             serverInfo["serverIP"] = serverIP;
 											if (serverInfo.maxPlayers <= 16 ) {
@@ -75,16 +68,24 @@ var jqhxr = $.getJSON( "http://192.99.124.162/list", null)
 														}
 													}
 													console.log(serverInfo);
-                                                    var serverInfo2 = serverInfo;
-                                                    delete serverInfo2.port;
-                                                    delete serverInfo2.players;
-                                                    delete serverInfo2.xnkid;
-                                                    delete serverInfo2.xnaddr;
-                                                    delete serverInfo2.gameVersion;
-													var array = $.map(serverInfo2, function(value, index) {
-														return [value];
-													});
-													t.row.add(array).draw();
+                                                    if(!serverInfo.hasOwnProperty("passworded")){
+                                                        serverInfo["passworded"] = false;
+                                                    };
+													table.row.add([
+                                                        serverInfo.serverId,
+                                                        serverInfo.serverIP,
+                                                        serverInfo.name,
+                                                        serverInfo.hostPlayer,
+                                                        serverInfo.map,
+                                                        serverInfo.mapFile,
+                                                        serverInfo.variant,
+                                                        serverInfo.variantType,
+                                                        serverInfo.status,
+                                                        serverInfo.numPlayers,
+                                                        serverInfo.maxPlayers,
+                                                        serverInfo.passworded,
+                                                        serverInfo.eldewritoVersion
+                                                    ]).draw();
 												} else {
 													console.log(serverInfo.serverIP + " is glitched");
 												}
