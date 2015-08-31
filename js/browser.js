fb1193a4557912a746b81ab3f695ac2a233c157e
scooterpsu <scooterpsu@gmail.com>
2015-08-31 10:34:59 -0400

    Bold the hostName, put it at the top
    Done

--- js/browser.js
+++ js/browser.js
@@ -439,9 +439,9 @@ function capitalizeFirstLetter(string) {
 function format ( d ) {
     var output = "";
     output += '<div id="leftside"><img id="mapPic" class="img-responsive" src="images/maps/' + serverList.servers[d[1]].mapFile + '.png"><img id="gameTypePic" class="img-responsive" src="images/gametypes/' + serverList.servers[d[1]].variantType + '.png">'+
-    '<h4 id="gameName">'+serverList.servers[d[1]].name+'</h4>'+
     '<h3 id="hostName">'+serverList.servers[d[1]].hostPlayer+'</h3>'+
-    '<h4 id="gameStatus">In '+serverList.servers[d[1]].status.split("In")[1]+'</h4></div>';
+    '<h4 id="gameStatus">In '+serverList.servers[d[1]].status.split("In")[1]+'</h4>'+
+    '<h4 id="gameName">'+serverList.servers[d[1]].name+'</h4></div>';
     if(!serverList.servers[d[1]].passworded){ 
         output += '<div id="rightside"><table class="statBreakdown"><thead class="tableHeader">'+
             '<th>Name</th>'+
