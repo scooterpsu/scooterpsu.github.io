2f0eb7b3f67ded1d45d17ae070dc722fa2e0d5c7
Scott Lewis <scooterpsu@gmail.com>
2015-12-31 17:31:31 -0500

    Disabled controller support, visual tweaks
    Inexplicably the controller bit is throwing the wrong index number. So
    it can't update the gamecard, and probably can't join.

--- js/browser.js
+++ js/browser.js
@@ -15,24 +15,6 @@ var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 $(document).ready(function() {
     buildTable();
-    // Add event listener for opening and closing details
-    $('#serverTable tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = $('#serverTable').DataTable().row( tr );
-        /*
-        if ( row.child.isShown() ) {
-            // This row is already open - close it
-            row.child.hide();
-            tr.removeClass('shown');
-        }
-
-        else {
-            // Open this row
-            row.child( expansionLine(row.data()) ).show();
-            tr.addClass('shown');
-        }
-        */
-    } );
 	window.addEventListener('resize', function(){
 		setTimeout(function() {
 			var infoPos = (window.innerWidth*0.48 - $('#serverTable_info').text().length*3 - 10);
@@ -45,24 +27,10 @@ $(document).ready(function() {
         
 function buildTable(){
 	$('#serverTable').on('click', 'tr', function() {
-		/*
-		console.log("ip: " + $(this).find('td:eq(0)').text());
-		console.log("numplayers: " + $(this).find('td:eq(8)').text());
-		console.log("maxplayers: " + $(this).find('td:eq(9)').text());
-		console.log("private: " + $(this).find('td:eq(10)').text());
-		console.log("version: " + $(this).find('td:eq(11)').text());
-		*/
-		//joinServer($(this).find('td:eq(0)').text(), $(this).find('td:eq(8)').text(), $(this).find('td:eq(9)').text(), $(this).find('td:eq(10)').text(), $(this).find('td:eq(11)').text());
 		var tr = $(this).closest('tr');
 		var row = table.row( tr );
 		if (row.data()){
 			joinServer(row.data()[0]);
-			/*
-            selectedID = jQuery(this).closest('tr').index();
-			selectedID++;
-			$('#serverTable tr.selected').removeClass('selected');
-			$("#serverTable tr:eq(" + selectedID + ")").addClass("selected");
-            */
 		} 
     });  
     $('#serverTable').on('mouseover', 'tr', function() {
@@ -73,7 +41,6 @@ function buildTable(){
         }
     });
   
-
 	var table = $('#serverTable').DataTable( {
 		"footerCallback": function ( row, data, start, end, display ) {
             var api = this.api(), data;
@@ -154,10 +121,6 @@ function buildTable(){
 																	serverList.servers[j] = serverInfo;
 																	serverCount++;
 																	playerCount+=serverInfo.numPlayers;
-																	//$('.serverCount').html(serverCount + " servers");
-																	//console.log(serverCount);
-																	//$('.playerCount').html(playerCount + " players");
-																	//console.log(playerCount);
 															}
 														}
 														//console.log(serverInfo);
@@ -166,11 +129,7 @@ function buildTable(){
 														} else {
                                                            serverInfo["passworded"] = "🔒";
                                                         };
-                                                        if (serverInfo.name.length > 45){
-                                                            serverInfo.name = serverInfo.name.substring(0, 42) + "..."
-                                                        };
 														table.row.add([
-															/*null,*/
 															serverInfo.serverId,
 															serverInfo.serverIP,
                                                             serverInfo.passworded,
@@ -281,8 +240,8 @@ function connectionTrigger(){
 }
 
 function disconnectTrigger(){
-    //$('.closeButton').hide();
-	//$('#serverTable_filter').css("right","10");
+    $('.closeButton').hide();
+	$('#serverTable_filter').css("right","10");
 }
 
 function closeBrowser() {
@@ -303,36 +262,14 @@ function updateSelection(){
 
 function joinSelected(){
 	var row = $('#serverTable').dataTable().fnGetData(selectedID-1);
-	//console.log(row[1]);
-	joinServer(row[1]);
+	joinServer(row[0]);
 }
 
 function toggleDetails(){
-    $('#serverTable tr:eq(' + selectedID + ') td.details-control').trigger( "click" );
+	var row = $('#serverTable').dataTable().fnGetData(selectedID-1);
+    console.log(row);
+	fillGameCard(row[0]);
 }
-
-/*
-function getLocation(ip, rowNum){ 
-    if (ip.contains(":")){
-        ip = ip.split(":")[0];
-    }
-    var geoInfo = $.getJSON("http://www.telize.com/geoip/" + ip, null )
-    .done(function(data) {
-        if ($('#serverTable').dataTable().fnGetData(rowNum, 3)=="Loading..."){
-            var location = "[" + data.continent_code + "] "
-            //if(data.city){location += data.city + ", "} //since sometimes data.city is missing
-            if(data.region){location += data.region + ", "} //since sometimes data.city is missing
-            if(location.length > 5){
-                location += data.country_code3;
-            } else {
-                location += data.country;
-            }
-            $('#serverTable').dataTable().fnUpdate(location, rowNum, 3);
-            $('#serverTable').DataTable().columns.adjust().draw();
-        }
-    });
-}
-*/
 
 function pingMe(ip, rowNum) {
     var startTime = Date.now();
@@ -375,57 +312,11 @@ function fillGameCard(i){
     }
 }
 
-/* Once you can pong via rcon
-function pingList(){ 
-    //fakePongs();
-    pingTable.length = 0;
-    $('#serverTable').dataTable().api().column( 3 ).visible( true );
-    var rowNum = 0;
-    var rowData = "";
-    var ip = "";
-    var ping = "";
-    while(rowNum <= $("#serverTable tbody tr").length-1){
-        rowData = $('#serverTable').dataTable().fnGetData(rowNum, 2);
-        if (rowData.contains(":")){
-            ip = rowData.split(":")[0];
-        }
-		if(dewRconConnected){
-			dewRcon.send('ping ' + ip);
-			if (dewRcon.lastMessage.length > 0) {
-				storePong(dewRcon.lastMessage);
-			} 
-		}
-        $('#serverTable').dataTable().fnUpdate(lookupPing(ip), rowNum, 3);  
-        rowNum++;
-    }
-    console.log(pingTable);
-}
-
-function storePong(reply){
-    if (reply.contains("PONG")){
-        pingTable.push(reply.split(" "));
-    }
-}
-
-function lookupPing(ip) {
-    for (var i=0; i<pingTable.length; i++)
-        if (pingTable[i][1] === ip && pingTable[i].length > 3)                    
-            return pingTable[i][3];
-    return "?";
-}
-
-function fakePongs(){
-    storePong("PONG 100.12.94.137 5849583405 14ms");
-    storePong("PONG 184.153.214.35 5849583405 26ms");
-    storePong("PONG 69.244.172.233 5849583405 4ms");
-    storePong("PONG 206.188.91.153 5849503859 20ms");
-}
-*/
-
 Mousetrap.bind('f11', function() {
     closeBrowser();
 });
 
+/*
 var gamepad = new Gamepad();
 
 gamepad.bind(Gamepad.Event.CONNECTED, function(device) {
@@ -445,7 +336,7 @@ gamepad.bind(Gamepad.Event.DISCONNECTED, function(device) {
 });
 
 gamepad.bind(Gamepad.Event.UNSUPPORTED, function(device) {
-    //console.log("an unsupported gamepad connected (add new mapping)");
+    console.log("an unsupported gamepad connected (add new mapping)");
 });
 
 gamepad.bind(Gamepad.Event.BUTTON_DOWN, function(e) {
@@ -514,69 +405,11 @@ gamepad.bind(Gamepad.Event.BUTTON_DOWN, function(e) {
 if (!gamepad.init()) {
     // Your browser does not support gamepads, get the latest Google Chrome or Firefox
 }
+*/
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-/*
-function expansionLine(d) {
-    var output = "";
-    output += '<div id="gamecard"><img id="mapPic" class="img-responsive" src="images/maps/' + serverList.servers[d[1]].mapFile + '.png">'+
-    '<img id="gameTypePic" class="img-responsive" src="images/gametypes/' + capitalizeFirstLetter(serverList.servers[d[1]].variantType) + '.png">'+
-    '<h4 id="hostName">'+serverList.servers[d[1]].hostPlayer+'</h4>'+
-    '<h5 id="gameStatus">In '+serverList.servers[d[1]].status.split("In")[1]+'</h5>'+
-    '<h5 id="gameName">'+serverList.servers[d[1]].name+'</h5>';
-    if (serverList.servers[d[1]].sprintEnabled == "1"){
-        if (serverList.servers[d[1]].sprintUnlimitedEnabled == "1") {
-            output +='<img id="sprintVars" src="images/infsprint.png">';
-        } else {
-             output +='<img id="sprintVars" src="images/sprint.png">'; 
-        }
-    }  else {
-             output +='<img id="sprintVars" src="images/nosprint.png">'; 
-    }
-    if (serverList.servers[d[1]].VoIP) {
-        output +='<img id="voipIcon" src="images/mic.png"></div>';
-    } else {
-        output +='<img id="voipIcon" src="images/nomic.png"></div>'; 
-    }
-    if(!serverList.servers[d[1]].passworded){ 
-        output += '<div id="scoreboard"><table class="statBreakdown"><thead class="tableHeader">'+
-            '<th>Name</th>'+
-            '<th><center>Score</center></th>'+
-            '<th><center>K</center></th>'+
-            '<th><center>D</center></th>'+
-            '<th><center>A</center></th>'+
-            '<th><center>Ratio</center></th>'+
-            '</thead><tbody>';
-        var playerNum = 0;
-            while (playerNum < serverList.servers[d[1]].players.length) {
-                var playerList = serverList.servers[d[1]].players;
-                playerList = sortByKey(playerList, 'score');
-                var ratio = 0;
-                if(playerList[playerNum].name){
-                    if(playerList[playerNum].kills>0){
-                        ratio = ((playerList[playerNum].kills+(playerList[playerNum].assists/3))/playerList[playerNum].deaths).toFixed(2);
-                    }
-                    output +=  '<tr>'+
-                        '<td class="statLines">'+playerList[playerNum].name+'</td>'+
-                        '<td class="statLines"><center>'+playerList[playerNum].score+'</center></td>'+
-                        '<td class="statLines"><center>'+playerList[playerNum].kills+'</center></td>'+
-                        '<td class="statLines"><center>'+playerList[playerNum].deaths+'</center></td>'+
-                        '<td class="statLines"><center>'+playerList[playerNum].assists+'</center></td>'+
-                        '<td class="statLines"><center>'+ratio+'</center></td>'+
-                    '</tr>';
-                }
-                playerNum++;
-            }
-        output += '</tbody></table></div>';  
-    } else {
-    output += "<div id='scoreboard' class='private'><center><h3>Private Game</h3><br /><h3>Player Data Unavailable</h3></center></div>";
-    }
-    return output;
-}
-*/
 
 function sortByKey(array, key) {
     return array.sort(function(b, a) {
