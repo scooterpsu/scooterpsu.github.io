5ca79e98e12c602892d5a07f789be201e2f7df86
scooterpsu <scooterpsu@gmail.com>
2016-01-25 10:31:59 -0500

    Temp 4k fix
    Will redo with media queries later

--- js/browser.js
+++ js/browser.js
@@ -1,3 +1,4 @@
+var zoomRatio;
 var mapList = [[]];
 var EDVersion = 0;
 var serverList = {
@@ -14,9 +15,15 @@ var selectedID = 0;
 var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 $(document).ready(function() {
+    fixResolution();
     getCurrentRelease();
     buildTable();
 });
+
+function fixResolution(){
+    zoomRatio = screen.width/1920;
+    $('body').css("zoom", zoomRatio);  
+}
 
 function getCurrentRelease() {
 	var fgjkfld = $.getJSON( "http://eldewrito.anvilonline.net/update.json", null)
