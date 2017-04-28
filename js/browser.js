bfebe9afec20239518d8d22b8f60926d79d5c3c1
scooterpsu <scooterpsu@gmail.com>
2017-04-27 22:35:12 -0400

    Updated path to dewrito.json

--- js/browser.js
+++ js/browser.js
@@ -21,7 +21,7 @@ var lastArray = [];
 var dewConnected = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 
-var dewritoURL = "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dewrito.json";
+var dewritoURL = "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dist/dewrito.json";
 swal.setDefaults({
     customClass: "alertWindow",
     html: true
