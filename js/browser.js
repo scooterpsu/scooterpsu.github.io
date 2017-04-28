a37e42627a1cc332b5446e39c14ca214155befb0
scooterpsu <scooterpsu@gmail.com>
2017-04-27 22:41:01 -0400

    New dewrito.json URL

--- js/browser.js
+++ js/browser.js
@@ -21,7 +21,7 @@ var selectedID = 0;
 var controllersOn = false;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 
-//var dewritoURL = "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dewrito.json";
+//var dewritoURL = "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dist/dewrito.json";
 var dewritoURL = "http://scooterpsu.github.io/dewrito.json";
 swal.setDefaults({
     customClass: "alertWindow",
