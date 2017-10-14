9f7aa2a54f90ff8df32760b5c97c93ba37065ac9
scooterpsu <scooterpsu@gmail.com>
2017-10-14 16:19:37 -0400

    Broken code is broken

--- new/js/browser.js
+++ new/js/browser.js
@@ -25,7 +25,6 @@ var lastHeldUpdated = 0;
 var VerifyIPRegex = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
 
 var dewritoURLList = [
-    "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dist/dewrito.json",
     "https://raw.githubusercontent.com/ElDewrito/ElDorito/master/dist/mods/dewrito.json",
     "http://scooterpsu.github.io/dewrito.json"
 ];
