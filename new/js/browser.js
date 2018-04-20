e79424e6e0b7bd17edb60cd64ff6f95afc21c8bd
scooterpsu <scooterpsu@gmail.com>
2018-04-19 20:55:25 -0400

    Revert "Test"
    This reverts commit 7b07b521fa5811ff56dbf0eeebbacd6f8e161546.

--- new/js/browser.js
+++ new/js/browser.js
@@ -69,7 +69,6 @@ $(document).ready(function() {
     $.getScript( "dew://lib/dew.js" )
     .done(function() {
         dewConnected = true;
-        dew.command('Game.FirstRun 1');
         dew.getVersion().then(function (version) {
             if(version.contains('-')){
                 gameVersion = version.split('-')[0];
