f5f6f5dd878bd4f52f1131f9d7709424199a6b2d
scooterpsu <scooterpsu@gmail.com>
2018-04-22 08:45:32 -0400

    Look closer before accepting PR's

--- new/js/browser.js
+++ new/js/browser.js
@@ -701,9 +701,9 @@ function oldBuildTable(server_list){
                                 dew.ping(serverInfo.serverIP.split(":")[0], serverInfo.port);
                             }
                             //checkOfficial(serverInfo.serverIP);
-                            if(!locked){
+                            /*if(!locked){
                                 getFlag(serverIP,$("#serverTable").DataTable().column(0).data().length-1);
-                            }
+                            }*/
                         } else {
                             console.log(serverInfo.serverIP + " is glitched");
                         }
