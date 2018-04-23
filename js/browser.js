db1594bcedb542678b564a39d4de5043d3d83911
unk-1 <unk0x4@gmail.com>
2018-04-23 06:35:26 +0100

    New browser redirect

--- js/browser.js
+++ js/browser.js
@@ -38,7 +38,7 @@ swal.setDefaults({
 $(document).ready(function() {
     $.getScript( "dew://lib/dew.js" )
     .done(function() {
-        window.location.href = '/new/';
+        window.location.href = 'http://rabidsquabbit.github.io/';
     })
     getCurrentRelease();
     initDewjson();
